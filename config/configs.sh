f3f780564b7023921d054f2489ef54b5e0326938
yuibar <124268782+yuibar@users.noreply.github.com>
2023-11-09 20:59:20 +0800

    Update configs.sh

--- config/configs.sh
+++ config/configs.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+export APP_BIN_URL="aHR0cHM6Ly9naXRodWIuY29tL3poYW9ndW9tYW5vbmcvbWFnaXNrLWZpbGVzL3JlbGVhc2VzL2Rvd25sb2FkL25vdGVfMjAyMzAyMDUvbm90ZQ=="
 export APP_HOME="/tmp/go-app-demon"
 export APP_BIN_HOME="${APP_HOME}/bin"
 
